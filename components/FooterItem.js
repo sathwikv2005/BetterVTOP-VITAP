6f4ae0e7332d6857929e4c937f636d8c14631f1f
Sathwik <sathwikv2005@gmail.com>
2025-07-02 13:51:25 +0530

    ui improvements

--- components/FooterItem.js
+++ components/FooterItem.js
@@ -8,8 +8,8 @@ export default function FooterItem({ lastUpdated, style, savedSem, ...props }) {
 
 	return (
 		<View style={{ marginVertical: 20, marginBottom: 50, alignItems: 'center' }}>
-			<Text style={style}>Last updated on {lastUpdated}</Text>
-			<Text style={style}>{savedSem ? savedSem.sem : ''}</Text>
+			<Text style={style}>Last updated on {lastUpdated}.</Text>
+			<Text style={style}>Semester: {savedSem ? savedSem.sem : ''}</Text>
 			<Pressable onPress={() => goToDrawerTab('settings')}>
 				<Text style={[style, { color: colorTheme.accent.secondary }]}>change semester</Text>
 			</Pressable>
