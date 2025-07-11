9f2207b5075bd3aafe6098b00865bdd78b37a8e2
Sathwik <sathwikv2005@gmail.com>
2025-07-11 12:07:37 +0530

    update

--- plugins/custom-gradle-fix.js
+++ plugins/custom-gradle-fix.js
@@ -2,17 +2,23 @@ const { withAppBuildGradle } = require('@expo/config-plugins')
 
 module.exports = function withCustomGradleFix(config) {
 	return withAppBuildGradle(config, (config) => {
-		config.modResults.contents = config.modResults.contents.replace(
-			/applicationVariants\.all[\s\S]*?}\n\s*}/,
-			`
-      applicationVariants.all { variant ->
-        if (variant.buildType.name == "release") {
-            variant.outputs.all { output ->
-                outputFileName = "BetterVTOP.apk"
-            }
+		const gradle = config.modResults.contents
+
+		// Avoid adding it multiple times
+		if (!gradle.includes('outputFileName = "BetterVTOP-${version}.apk"')) {
+			config.modResults.contents += `
+
+android.applicationVariants.all { variant ->
+    if (variant.buildType.name == "release") {
+        variant.outputs.all { output ->
+            def version = variant.versionName
+            outputFileName = "BetterVTOP-\${version}.apk"
         }
-      }`
-		)
+    }
+}
+`
+		}
+
 		return config
 	})
 }
