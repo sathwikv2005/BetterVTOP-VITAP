001f24968170e0b6470486969251877b0d06bc81
Sathwik <sathwikv2005@gmail.com>
2025-07-13 23:39:00 +0530

    (android) ask for notification permission

--- plugins/withInstallPermission.js
+++ plugins/withInstallPermission.js
@@ -5,15 +5,19 @@ module.exports = function withInstallPermission(config) {
 		const manifest = config.modResults
 		const permissions = manifest.manifest['uses-permission'] || []
 
-		const permissionName = 'android.permission.REQUEST_INSTALL_PACKAGES'
+		const requiredPermissions = [
+			'android.permission.REQUEST_INSTALL_PACKAGES',
+			'android.permission.POST_NOTIFICATIONS',
+		]
 
-		const alreadyExists = permissions.some((p) => p.$['android:name'] === permissionName)
-
-		if (!alreadyExists) {
-			permissions.push({
-				$: { 'android:name': permissionName },
-			})
-		}
+		requiredPermissions.forEach((permissionName) => {
+			const alreadyExists = permissions.some((p) => p.$['android:name'] === permissionName)
+			if (!alreadyExists) {
+				permissions.push({
+					$: { 'android:name': permissionName },
+				})
+			}
+		})
 
 		manifest.manifest['uses-permission'] = permissions
 		return config
