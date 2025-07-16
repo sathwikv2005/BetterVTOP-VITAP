adf1c05f799429dd0d6275022efa2b541c7c0bb9
Sathwik <sathwikv2005@gmail.com>
2025-07-16 14:39:06 +0530

    feat: add support for more file extension mappings for downloads

---
+++ util/mimeToExtension.js
@@ -0,0 +1,55 @@
+export default {
+	// Documents
+	'application/pdf': 'pdf',
+	'application/msword': 'doc',
+	'application/vnd.openxmlformats-officedocument.wordprocessingml.document': 'docx',
+	'application/vnd.ms-excel': 'xls',
+	'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': 'xlsx',
+	'application/vnd.ms-powerpoint': 'ppt',
+	'application/vnd.openxmlformats-officedocument.presentationml.presentation': 'pptx',
+	'application/rtf': 'rtf',
+	'text/plain': 'txt',
+	'text/csv': 'csv',
+	'application/epub+zip': 'epub',
+
+	// Archives
+	'application/zip': 'zip',
+	'application/x-rar-compressed': 'rar',
+	'application/x-7z-compressed': '7z',
+	'application/x-tar': 'tar',
+	'application/gzip': 'gz',
+
+	// Images
+	'image/png': 'png',
+	'image/jpeg': 'jpg',
+	'image/jpg': 'jpg',
+	'image/gif': 'gif',
+	'image/bmp': 'bmp',
+	'image/webp': 'webp',
+	'image/svg+xml': 'svg',
+	'image/heif': 'heif',
+	'image/heic': 'heic',
+
+	// Audio
+	'audio/mpeg': 'mp3',
+	'audio/wav': 'wav',
+	'audio/ogg': 'ogg',
+	'audio/mp4': 'm4a',
+	'audio/x-ms-wma': 'wma',
+	'audio/webm': 'weba',
+
+	// Video
+	'video/mp4': 'mp4',
+	'video/x-msvideo': 'avi',
+	'video/x-matroska': 'mkv',
+	'video/webm': 'webm',
+	'video/quicktime': 'mov',
+
+	// Code
+	'application/json': 'json',
+	'application/javascript': 'js',
+	'text/html': 'html',
+	'text/css': 'css',
+	'text/markdown': 'md',
+	'application/xml': 'xml',
+}
